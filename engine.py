fde0d30245f88bdfc9976944c1f4a06bbe1ff0f3
Gallenmar <67636595+Gallenmar@users.noreply.github.com>
2020-07-01 10:36:47 +0000

    Car v0.0
    Basics are done

--- engine.py
+++ engine.py
@@ -4,66 +4,53 @@ import array as arr
 pygame.init()
 
 FRAMERATE = 400
-JUMPHEIGHT = 300
-XSTEP = 2  # how much alien moves side to side
-WIDTH = 532; HEIGHT = 850 # size of the screen
-XDIM = 50; YDIM = 10 # X and Y dimention (size) of the blocks
-QUANTITY = 20 # QUANTITY of blocks
-UPSPEED = 2 #speed of the screen, but less is faster, 1 - not playeble
+#JUMPHEIGHT = 300
+#XSTEP = 2  # how much alien moves side to side
+WIDTH = 532; HEIGHT = 850
+#XDIM = 50; YDIM = 10 # X and Y dimention (size) of the blocks
+#QUANTITY = 20 # QUANTITY of blocks
+#UPSPEED = 2 #speed of the screen, but less is faster, 1 - not playeble
 BGCOLOR = pygame.Color('black')
 FGCOLOR = pygame.Color('white')
-ALCOLOR = pygame.Color('red')
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT)) # creating screen
+#ALCOLOR = pygame.Color('red')
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN.fill(BGCOLOR)
 
-class nedoAlien:
-    def __init__(self,x,y):
-        self.x = x
-        self._y = y
-    RADIUS = 10
-    speed = 1
-    pixelsJumped = 0
+class Car:
+    _carWidth = 10
+    _carHeight = 20
+    _carspeed = 1
 
-class Alien(nedoAlien):
     def __init__(self,x,y):
-        self.x = x
+        self._x = x
         self._y = y
     def show(self, color):
-        global SCREEN #telling python you are using global var
-        pygame.draw.circle(SCREEN, color, (self.x, self._y), self.RADIUS)
+        global SCREEN
+        pygame.draw.rect(
+            SCREEN, color, pygame.Rect(self._x, self._y, self._carWidth, self._carHeight))
     def move(self):
         global JUMPHEIGHT
         self.show(BGCOLOR)
         self._y += self.speed
-        self.show(ALCOLOR)
+        self.show(FGCOLOR)
         if not self.falls(): 
             self.pixelsJumped +=1
         if self.pixelsJumped == JUMPHEIGHT:
             self.speed = 1
             self.pixelsJumped = 0
-    def lowerBorderY(self):
-        return self._y + self.RADIUS
-    def falls(self):
-        if self.speed > 0 :
-            return True
-        else:
-            return False
-    def jump(self):
-        self.speed = -1
-        self.pixelsJumped = 0
-    def inTheZone(self, x1, dim): # x1 left boundry         dim is width
-        if (self.x + self.RADIUS)> x1 and (self.x - self.RADIUS) < (x1 + dim):
-            return True
-        else:
-            return False
     def left(self):
-        global ALCOLOR, BGCOLOR
         self.show(BGCOLOR)
-        self.x -= XSTEP
-        self.show(ALCOLOR)
+        self._x -= self._carspeed
+        self.show(FGCOLOR)
     def right(self):
-        global ALCOLOR, BGCOLOR
         self.show(BGCOLOR)
-        self.x += XSTEP
-        self.show(ALCOLOR)
-    def retY(self):
-        return self._y
+        self._x += self._carspeed
+        self.show(FGCOLOR)
+    def up(self):
+        self.show(BGCOLOR)
+        self._y -= self._carspeed
+        self.show(FGCOLOR)
+    def down(self):
+        self.show(BGCOLOR)
+        self._y += self._carspeed
+        self.show(FGCOLOR)
